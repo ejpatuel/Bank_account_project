0ae4e7c0f2543d660fa184fdb607b6a3848285d5
ejpatuel <ejpatuel@gmail.com>
2025-04-14 12:30:04 -0700

    Set the curses window to be as large as the terminal will allow

--- account/curse_ui.py
+++ account/curse_ui.py
@@ -7,13 +7,13 @@ stdscr.keypad(True)
 
 def main(stdscr):
   stdscr.clear()
-  begin_x = 20; begin_y = 7
+  begin_x = 0; begin_y = 0
   height = 5; width = 40
-  win = curses.newwin(height, width, begin_y, begin_x)
+  win = curses.newwin(curses.LINES - 1, curses.COLS - 1, begin_y, begin_x)
 
 
   win.clear()
-  win.addstr(0, 0, 'Welcome, I see you are new to our bank, would you like to create a bank account?\n [Y/N]: ')
+  win.addstr(1, 1, 'Welcome, I see you are new to our bank, would you like to create a bank account?\n [Y/N]: ')
   win.refresh()
   win.box()
   win.getkey()
